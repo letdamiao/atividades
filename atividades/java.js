e0baabe63e4758a35ce5ea115c793f01e4f0f2d7
Letícia Damião <108810404+letdamiao@users.noreply.github.com>
2023-04-15 11:43:59 -0300

    Add files via upload

---
+++ atividades/java.js
@@ -0,0 +1,14 @@
+//const nome =["Lê", "vi"];
+//nome.pop();
+//console.log(nome);
+
+//const nome =["lê", "vi"];
+//nome.unshift("lele");
+//console.log(nome);
+
+//const nome =["lê", "vi"];
+//nome.push("lele");
+//console.log(nome);
+
+
+, 
